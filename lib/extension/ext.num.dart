0134b5f4e9a2ad67cb070ff4fe45bd25874e8235
yeji <yejipooh@gmail.com>
2025-08-06 10:57:44 +0900

    feat: pad ext

--- lib/extension/ext.num.dart
+++ lib/extension/ext.num.dart
@@ -10,3 +10,69 @@ extension GapExtension on num {
 extension SliverGapExtension on num {
   SliverGap get sg => SliverGap(toDouble());
 }
+
+extension SliverPaddingExtension on num {
+  SliverPadding sliverPad({
+    required Widget sliver,
+    double? horizontal,
+    double? vertical,
+    double? top,
+    double? bottom,
+    double? left,
+    double? right,
+  }) {
+    final double value = toDouble();
+
+    final EdgeInsets padding;
+
+    if (horizontal != null || vertical != null) {
+      padding = EdgeInsets.symmetric(
+        horizontal: horizontal ?? 0,
+        vertical: vertical ?? 0,
+      );
+    } else if (top != null || bottom != null || left != null || right != null) {
+      padding = EdgeInsets.only(
+        top: top ?? 0,
+        bottom: bottom ?? 0,
+        left: left ?? 0,
+        right: right ?? 0,
+      );
+    } else {
+      padding = EdgeInsets.all(value); // ✅ 기본값
+    }
+
+    return SliverPadding(padding: padding, sliver: sliver);
+  }
+}
+
+extension PaddingExtension on num {
+  Padding pad({
+    required Widget child,
+    double? horizontal,
+    double? vertical,
+    double? top,
+    double? bottom,
+    double? left,
+    double? right,
+  }) {
+    EdgeInsets padding;
+
+    if (horizontal != null || vertical != null) {
+      padding = EdgeInsets.symmetric(
+        horizontal: horizontal ?? 0,
+        vertical: vertical ?? 0,
+      );
+    } else if (top != null || bottom != null || left != null || right != null) {
+      padding = EdgeInsets.only(
+        top: top ?? 0,
+        bottom: bottom ?? 0,
+        left: left ?? 0,
+        right: right ?? 0,
+      );
+    } else {
+      padding = EdgeInsets.all(toDouble());
+    }
+
+    return Padding(padding: padding, child: child);
+  }
+}
